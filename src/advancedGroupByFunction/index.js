5a2e009e9cc65a373518d319accdf88413721590
Manish <manish@gmail.com>
2022-11-13 22:25:57 +0545

    starting sub_total

--- src/advancedGroupByFunction/index.js
+++ src/advancedGroupByFunction/index.js
@@ -5,7 +5,8 @@ const AdvancedGroup = () => {
   const arr = people;
   var groups = ["lotNo"];
   var grouped = { groupByLotNo: {} };
-  let newArr = [];
+  let headerContentArr = [];
+  let getPremiumSubtoal = [];
 
   //********************************************************************
   //GROUPS BY LOTNO
@@ -30,7 +31,6 @@ const AdvancedGroup = () => {
   let group_By_headerContent = Object.entries(grouped.groupByLotNo);
   group_By_headerContent.forEach((item) => {
     let getLotNo = item[0];
-
     let get_header_Content_Name = ["doc", "maturityDate", "lotNo"];
 
     return item[1].slice(0, 1).forEach((itemName) => {
@@ -41,12 +41,42 @@ const AdvancedGroup = () => {
           ...obj,
         };
       }, {});
-      newArr = newArr.concat({ [getLotNo]: [renObject] });
+      // headerContentArr = headerContentArr.concat({ [getLotNo]: [renObject] });
+      headerContentArr.push({ [getLotNo]: [renObject] });
     });
   });
-  console.log("********************");
-  console.log(typeof newArr);
-  console.log(newArr);
+
+  grouped = Object.assign(grouped, { groupByHeaderContent: headerContentArr });
+  // console.log("********************");
+  // console.log(grouped);
+
+  //********************************************************************
+  //GROUPS BY SUBTOTAL
+  //*********************************************************************/
+
+  group_By_headerContent.forEach(function (item) {
+    let getLotNo = item[0];
+
+    let return_Subtotal = item[1].reduce(function (o, g, i) {
+      return o + Math.round(g["premium"]);
+    }, 0);
+
+    // getPremiumSubtoal = getPremiumSubtoal.concat({
+    //   [getLotNo]: [return_Subtotal],
+    // });
+    getPremiumSubtoal.push({ [getLotNo]: [return_Subtotal] });
+  });
+  Object.assign(grouped, { getPremiumSubtoal: getPremiumSubtoal });
+  console.log(grouped);
+
+  //********************************************************************
+  //CALCULATION TOTAL FROM SUB_TOTAL
+  //*********************************************************************/
+  let calc_total = Object.entries(grouped.getPremiumSubtoal);
+  calc_total.forEach(function (item) {
+    debugger;
+    console.log(item[1]);
+  });
 
   return <div>Manish</div>;
 };
